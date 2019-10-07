9b6203a4bdcdf4944608986f51424f1dbff0fb2b
Sangwon Lee <gamzabaw@gmail.com>
2019-10-07 11:58:06 +0900

    bump up version to 0.0.2

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
   long_description=long_description,
   author="Sangwon Lee",
   author_email="gamzabaw@gmail.com",
-  version="0.0.1",
+  version="0.0.2",
   license="MIT License",
   zip_safe=False,
   include_package_data=True,
